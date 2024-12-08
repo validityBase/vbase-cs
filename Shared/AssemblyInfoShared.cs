33c8e9b43ae5c21d8c4c112df6eeedc6bddcf94c
Volodymyr Bobko <volodymyrbobko@gmail.com>
2024-12-08 11:24:55 +0100

    Initial project structure

---
+++ Shared/AssemblyInfoShared.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("validityBase")]
+[assembly: AssemblyProduct("vBase SDK")]
+[assembly: AssemblyCopyright("Copyright © 2024, validityBase, www.vbase.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
