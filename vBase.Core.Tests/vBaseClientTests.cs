3ffb6a4942c566290782f220b43f7a6a323d60ee
Volodymyr Bobko <volodymyrbobko@gmail.com>
2024-12-13 18:30:42 +0100

    misc refactoring

--- vBase.Core.Tests/vBaseClientTests.cs
+++ vBase.Core.Tests/vBaseClientTests.cs
@@ -5,17 +5,28 @@ namespace vBase.Core.Tests;
 [TestFixture("https://dev.api.vbase.com/forwarder/",
   "hPnKt94hz2CbZMmj6iz_4tWV0q21hQ3JOif02hOu6UU",
   "0x4d22553b6559103d337144874ce13489583de4a12516b0575840c0d6199cb296")]
-public class vBaseClientTests(string ForwarderUrl, string ApiKey, string PrivateKey)
+public class vBaseClientTests
 {
   private vBaseClient _client;
+
+  private readonly string _forwarderUrl;
+  private readonly string _apiKey;
+  private readonly string _privateKey;
+
+  public vBaseClientTests(string forwarderUrl, string apiKey, string privateKey)
+  {
+    _forwarderUrl = forwarderUrl;
+    _apiKey = apiKey;
+    _privateKey = privateKey;
+  }
 
   [SetUp]
   public void Setup()
   {
     var commitmentService = CommitmentServiceBuilder.BuildForwarderCommitmentService(
-      ForwarderUrl,
-      ApiKey,
-      PrivateKey
+      _forwarderUrl,
+      _apiKey,
+      _privateKey
     );
 
     _client = new vBaseClient(commitmentService);
