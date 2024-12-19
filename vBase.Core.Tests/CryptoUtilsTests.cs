3b878142fa279b3e82f3b03a245027a02eadc77d
Volodymyr Bobko <volodymyrbobko@gmail.com>
2024-12-19 23:21:25 +0100

    Update test cases for compatibility with Py vBase SDK by enhancing naming conventions

--- vBase.Core.Tests/CryptoUtilsTests.cs
+++ vBase.Core.Tests/CryptoUtilsTests.cs
@@ -8,15 +8,18 @@ namespace vBase.Core.Tests;
 
 public class CryptoUtilsTests
 {
-  [TestCase("TestRecord", "0x5cbda6a837e3c6aebd2ce2f14510d4234b870d415ac06f4e411d995504232d2d", TestName = "String Hashing")]
+  [TestCase("TestRecord", "0x5cbda6a837e3c6aebd2ce2f14510d4234b870d415ac06f4e411d995504232d2d",
+    TestName = "String Hashing. Py vBase SDK Compatibility Check.")]
   public void GetObjectCid_CompatibilityWithPy_vBaseSdk_Test(object data, string cid)
   {
     data.GetCid().ToHex(true).Should().Be(cid);
   }
 
 
-  [TestCase(1968374, "0xbef23e694ee8fa4edce2a021c31b070ebbde7f36942fc3c7999bd01509bf3f69", 256u, TestName = "Int 256 Hashing")]
-  [TestCase(1968374, "0x85f62bc4cb22ef8d59817dafc9fbf13222f18a114834437524478eb105f1d6aa", 64u, TestName = "Int 64 Hashing")]
+  [TestCase(1968374, "0xbef23e694ee8fa4edce2a021c31b070ebbde7f36942fc3c7999bd01509bf3f69", 256u,
+    TestName = "Int 256 Hashing. Py vBase SDK Compatibility Check.")]
+  [TestCase(1968374, "0x85f62bc4cb22ef8d59817dafc9fbf13222f18a114834437524478eb105f1d6aa", 64u,
+    TestName = "Int 64 Hashing. Py vBase SDK Compatibility Check.")]
   public void GetIntCid_CompatibilityWithPy_vBaseSdk_Test(int data, string cid, uint size)
   {
     BigInteger.Parse(data.ToString())
