c95f2e2585d092f09adb03e983e450435c88eaef
Volodymyr Bobko <volodymyrbobko@gmail.com>
2024-12-19 23:09:51 +0100

    Enhance CID compatibility with Py SDK by adding integer hashing support and refactoring GetCid methods

--- vBase.Core.Tests/CryptoUtilsTests.cs
+++ vBase.Core.Tests/CryptoUtilsTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 using FluentAssertions;
 using Nethereum.Hex.HexConvertors.Extensions;
 using vBase.Core.Utilities;
@@ -7,11 +8,23 @@ namespace vBase.Core.Tests;
 
 public class CryptoUtilsTests
 {
-  [TestCase("TestRecord", "0x5cbda6a837e3c6aebd2ce2f14510d4234b870d415ac06f4e411d995504232d2d")]
-  [TestCase(1, "0xb79151ec5d30a80b78789805f293fa4fb8fd1eebc0c9367e7c9106678a893df1")]
-  [TestCase(1968374, "0xbef23e694ee8fa4edce2a021c31b070ebbde7f36942fc3c7999bd01509bf3f69")]
-  public void GetCid_CompatibilityWithPy_vBaseSdk_Test(object data, string cid)
+  [TestCase("TestRecord", "0x5cbda6a837e3c6aebd2ce2f14510d4234b870d415ac06f4e411d995504232d2d", TestName = "String Hashing")]
+  public void GetObjectCid_CompatibilityWithPy_vBaseSdk_Test(object data, string cid)
   {
     data.GetCid().ToHex(true).Should().Be(cid);
   }
+
+
+  [TestCase(1968374, "0xbef23e694ee8fa4edce2a021c31b070ebbde7f36942fc3c7999bd01509bf3f69", 256u, TestName = "Int 256 Hashing")]
+  [TestCase(1968374, "0x85f62bc4cb22ef8d59817dafc9fbf13222f18a114834437524478eb105f1d6aa", 64u, TestName = "Int 64 Hashing")]
+  public void GetIntCid_CompatibilityWithPy_vBaseSdk_Test(int data, string cid, uint size)
+  {
+    BigInteger.Parse(data.ToString())
+      .GetCid(size)
+      .ToHex(true)
+      .Should()
+      .Be(cid);
+  }
+
+
 }
--- vBase.Core/Utilities/CryptoUtils.cs
+++ vBase.Core/Utilities/CryptoUtils.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using System.Text;
-using Nethereum.ABI;
 using Nethereum.Util;
 using Nethereum.Web3.Accounts;
 using Org.BouncyCastle.Crypto.Digests;
@@ -10,16 +10,40 @@ namespace vBase.Core.Utilities;
 
 public static class CryptoUtils
 {
-  private static readonly ABIEncode AbiEncode = new();
-
+  /// <summary>
+  /// Get SHA3 256 hash of the input data.
+  /// </summary>
+  /// <param name="value">input data</param>
+  /// <returns>SHA3 256 hash bytes</returns>
   public static byte[] GetCid(this object value)
   {
     byte[] input = GetBytesFromData(value);
-    Sha3Digest sha3 = new Sha3Digest(256);
-    sha3.BlockUpdate(input, 0, input.Length);
-    byte[] result = new byte[sha3.GetDigestSize()];
-    sha3.DoFinal(result, 0);
-    return result;
+    return GetSha3Hash(input);
+  }
+
+  /// <summary>
+  /// Get SHA3 256 hash of the input integer.
+  /// </summary>
+  /// <param name="value">Integer value.</param>
+  /// <param name="size">Size in bits.</param>
+  /// <returns>SHA3 256 hash bytes</returns>
+  public static byte[] GetCid(this BigInteger value, uint size = 256)
+  {
+    if (size % 8 != 0)
+    {
+      throw new ArgumentException("Size must be a multiple of 8.");
+    }
+
+    var sizeInBytes = size / 8;
+
+    byte[] intBytes = value.ToByteArray().Reverse().ToArray();
+    if (intBytes.Length > sizeInBytes)
+    {
+      throw new ArgumentException($"Integer value {value} is too large for {size} bits.");
+    }
+    byte[] intResult = new byte[sizeInBytes];
+    intBytes.CopyTo(intResult, sizeInBytes - intBytes.Length);
+    return GetSha3Hash(intResult);
   }
 
   public static string ChecksumAddress(this Account account)
@@ -27,19 +51,20 @@ public static class CryptoUtils
     return AddressUtil.Current.ConvertToChecksumAddress(account.Address);
   }
 
+  private static byte[] GetSha3Hash(byte[] input)
+  {
+    Sha3Digest sha3 = new Sha3Digest(256);
+    sha3.BlockUpdate(input, 0, input.Length);
+    byte[] result = new byte[sha3.GetDigestSize()];
+    sha3.DoFinal(result, 0);
+    return result;
+  }
+
   private static byte[] GetBytesFromData(object data)
   {
     if (data is string dataStr)
     {
       return Encoding.UTF8.GetBytes(dataStr);
-    }
-
-    if (data is int dataInt)
-    {
-      var intBytes = BitConverter.GetBytes(dataInt).Reverse().ToArray();
-      byte[] intResult = new byte[32];
-      intBytes.CopyTo(intResult, 32 - intBytes.Length);
-      return intResult;
     }
 
     throw new NotSupportedException($"Data type {data.GetType()} is not supported.");
