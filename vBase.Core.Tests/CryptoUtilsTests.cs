dadb49481c71ed9ca46b1acf8a541a7ce946b662
Volodymyr Bobko <volodymyrbobko@gmail.com>
2024-12-19 22:29:40 +0100

    fixing CID compatibility with Py SDK

---
+++ vBase.Core.Tests/CryptoUtilsTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using FluentAssertions;
+using Nethereum.Hex.HexConvertors.Extensions;
+using vBase.Core.Utilities;
+
+namespace vBase.Core.Tests;
+
+public class CryptoUtilsTests
+{
+  [TestCase("TestRecord", "0x5cbda6a837e3c6aebd2ce2f14510d4234b870d415ac06f4e411d995504232d2d")]
+  [TestCase(1, "0xb79151ec5d30a80b78789805f293fa4fb8fd1eebc0c9367e7c9106678a893df1")]
+  [TestCase(1968374, "0xbef23e694ee8fa4edce2a021c31b070ebbde7f36942fc3c7999bd01509bf3f69")]
+  public void GetCid_CompatibilityWithPy_vBaseSdk_Test(object data, string cid)
+  {
+    data.GetCid().ToHex(true).Should().Be(cid);
+  }
+}
--- vBase.Core/Utilities/CryptoUtils.cs
+++ vBase.Core/Utilities/CryptoUtils.cs
@@ -1,5 +1,10 @@
-﻿using Nethereum.ABI;
+﻿using System;
+using System.Linq;
+using System.Text;
+using Nethereum.ABI;
+using Nethereum.Util;
 using Nethereum.Web3.Accounts;
+using Org.BouncyCastle.Crypto.Digests;
 
 namespace vBase.Core.Utilities;
 
@@ -9,11 +14,34 @@ public static class CryptoUtils
 
   public static byte[] GetCid(this object value)
   {
-    return AbiEncode.GetSha3ABIEncoded(value);
+    byte[] input = GetBytesFromData(value);
+    Sha3Digest sha3 = new Sha3Digest(256);
+    sha3.BlockUpdate(input, 0, input.Length);
+    byte[] result = new byte[sha3.GetDigestSize()];
+    sha3.DoFinal(result, 0);
+    return result;
   }
 
   public static string ChecksumAddress(this Account account)
   {
-    return Nethereum.Util.AddressUtil.Current.ConvertToChecksumAddress(account.Address);
+    return AddressUtil.Current.ConvertToChecksumAddress(account.Address);
+  }
+
+  private static byte[] GetBytesFromData(object data)
+  {
+    if (data is string dataStr)
+    {
+      return Encoding.UTF8.GetBytes(dataStr);
+    }
+
+    if (data is int dataInt)
+    {
+      var intBytes = BitConverter.GetBytes(dataInt).Reverse().ToArray();
+      byte[] intResult = new byte[32];
+      intBytes.CopyTo(intResult, 32 - intBytes.Length);
+      return intResult;
+    }
+
+    throw new NotSupportedException($"Data type {data.GetType()} is not supported.");
   }
 }
