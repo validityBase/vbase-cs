cd109470831e9ed32a576b47f3cee3d7575237cb
Volodymyr Bobko <volodymyrbobko@gmail.com>
2025-01-08 18:39:01 +0100

    working on docs publishing

--- build-repo-docs-action/src/index.ts
+++ build-repo-docs-action/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Custom action ran!');
+const { exec } = require('child_process');
+import * as core from '@actions/core';
+
+console.log('Publishing user documentation to the central docs repository...');
+
+const docsRepoAccessToken = core.getInput('docs-repo-access-token');
+const docsRepository = core.getInput('docs-repository');
+
+// checkout the docs repository
+console.log(`Cloning the docs repository ${docsRepository}...`);
+exec(`git clone https://${docsRepoAccessToken}@github.com/${docsRepository}.git main-docs`);
+exec("cd main-docs");
+console.log('Cloning the docs repository done.');
+
+// copy the markdown files from the build directory to the docs repository
+
+// commit and push the changes to the docs repository
