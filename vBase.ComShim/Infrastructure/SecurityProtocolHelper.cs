bfc7d7bb8d9b0a5bf1c4eff0b9d727f8f95acca0
Volodymyr Bobko <volodymyrbobko@gmail.com>
2025-01-13 22:42:37 +0100

    fix for connectivity issue

---
+++ vBase.ComShim/Infrastructure/SecurityProtocolHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace vBase.Infrastructure
+{
+  public static class SecurityProtocolHelper
+  {
+    /// <summary>
+    /// When running the shim in the VBA environment, we observed on some machines
+    /// that the security protocol is explicitly set to Ssl3 or Tls.
+    /// Such a configuration is incompatible with TLS 1.2, which is the protocol used by the Forwarder server.
+    /// Microsoft recommends using SystemDefault and avoiding explicitly specifying protocols.
+    /// For more details, see: https://docs.microsoft.com/en-us/dotnet/framework/network-programming/tls
+    /// </summary>
+    public static void ResetSecurityProtocol()
+    {
+      ServicePointManager.SecurityProtocol = 0; // set to SystemDefault
+    }
+  }
+}
