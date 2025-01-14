e1cb144be39a82a5bed22a4943f13bdde0c6c4fa
Volodymyr Bobko <volodymyrbobko@gmail.com>
2025-01-14 19:09:52 +0100

    Set SecurityProtocol explicitly to Tls12

---
+++ vBase.ComShim/Infrastructure/SecurityProtocolHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+
+namespace vBase.Infrastructure
+{
+  [ComVisible(false)]
+  public static class SecurityProtocolHelper
+  {
+    /// <summary>
+    /// When running the shim in the VBA environment, we observed on some machines
+    /// that the security protocol is explicitly set to Ssl3 or Tls.
+    /// Such a configuration is incompatible with TLS 1.2, which is the protocol used by the Forwarder server.
+    /// Experimentally, we found that setting the security protocol to 0 (SystemDefault) does not resolve the issue.
+    /// Setting explicitly to Tls12 does.
+    /// </summary>
+    public static void ResetSecurityProtocol()
+    {
+      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+    }
+  }
+}
