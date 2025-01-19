522f99353c74d1a0028ec4b6b2bfdca567f30aeb
Volodymyr Bobko <volodymyrbobko@gmail.com>
2025-01-19 18:13:40 +0100

    fix build

--- vBase.Core/Dataset/vBaseObjects/vBaseObject.cs
+++ vBase.Core/Dataset/vBaseObjects/vBaseObject.cs
@@ -15,13 +15,32 @@ public abstract class vBaseObject
 
   public vBaseObject() { }
 
+  /// <summary>
+  /// The data stored in the object.
+  /// </summary>
   public object? Data { get; protected set; }
 
+  /// <summary>
+  /// String representation of the data.
+  /// </summary>
   public string? StringData => (string?)Data;
 
+  /// <summary>
+  /// Initializes the object from a JSON object.
+  /// </summary>
+  /// <param name="jData">Json value.</param>
   public abstract void InitFromJson(JValue? jData);
 
+
+  /// <summary>
+  /// Serializes the object to a JSON value.
+  /// </summary>
+  /// <returns></returns>
   public abstract JValue? GetJson();
 
+  /// <summary>
+  /// Returns the <see cref="Cid"/> of the object.
+  /// </summary>
+  /// <returns>CID (Content Identifiers) for the current object</returns>
   public abstract Cid GetCid();
 }
