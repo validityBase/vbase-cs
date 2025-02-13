098ad6cbc09577820bd5feff630d2cca40299e17
Volodymyr Bobko <volodymyr-bobko@users.noreply.github.com>
2025-02-13 10:30:12 +0100

    Update patch-ms.js

--- .github/workflows/patch-ms.js
+++ .github/workflows/patch-ms.js
@@ -16,7 +16,7 @@ fs.readdirSync(dir).forEach(file => {
             .replace(/\|\|\|([^|]+)\|\|\|/gm, (_, p1) => {
                 if(p1.startsWith('http')) return p1; // ignore external links
                 console.log('\tEscaping: ' + p1);
-                return encodeURIComponent(p1.toLowerCase());
+                return encodeURIComponent(p1.toLowerCase().replace(',').replace('-').replace('`').replace("'").replace("(").replace(")"));
             });
 
         fs.writeFileSync(filePath, result, 'utf8');
